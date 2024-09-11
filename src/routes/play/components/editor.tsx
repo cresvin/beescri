fb2838643a9c6350c28d7b9f2e4a78e012a3d8a3
cresvin <oliwiertkacz2@gmail.com>
2024-09-11 15:51:39 +0200

    Add custom theme to monaco editor

--- src/routes/play/components/editor.tsx
+++ src/routes/play/components/editor.tsx
@@ -1,4 +1,8 @@
-import { Editor as MonacoEditor, OnChange } from "@monaco-editor/react";
+import {
+  Editor as MonacoEditor,
+  OnChange,
+  OnMount,
+} from "@monaco-editor/react";
 
 interface EditorProps {
   value: string;
@@ -7,9 +11,37 @@ interface EditorProps {
 }
 
 export const Editor = ({ value, onChange, language }: EditorProps) => {
+  const handleEditorDidMount: OnMount = (_, monaco) => {
+    monaco.editor.defineTheme("beescri", {
+      base: "vs-dark",
+      inherit: true,
+      rules: [],
+      colors: {
+        "editor.background": "#020611",
+        "editor.foreground": "#f8f9fa",
+        "editorWidget.background": "#020611",
+        "editorWidget.foreground": "#f8f9fa",
+        "editor.lineHighlightBackground": "#0f1722",
+        "input.background": "#020611",
+        "input.foreground": "#f8f9fa",
+        "dropdown.background": "#020611",
+        "dropdown.foreground": "#f8f9fa",
+        "dropdown.border": "#1f2937",
+        "scrollbarSlider.background": "#1f2937",
+        "scrollbarSlider.hoverBackground": "#374151",
+        "scrollbarSlider.activeBackground": "#4b5563",
+        "editorLineNumber.foreground": "#6b7280",
+        "editorLineNumber.activeForeground": "#f8f9fa",
+        "editor.selectionBackground": "#264f78",
+      },
+    });
+    monaco.editor.setTheme("beescri");
+  };
+
   return (
     <MonacoEditor
       theme="vs-dark"
+      onMount={handleEditorDidMount}
       value={value}
       onChange={onChange}
       language={language}
